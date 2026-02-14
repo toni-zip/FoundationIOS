1559089bceea6da03f004b859fe2a2dad84e58e9
jpaveiro <jpaveiros@gmail.com>
2026-02-14 16:17:47 -0300

    fix: corrige background cortado

--- Pokedex/View/ProfileView.swift
+++ Pokedex/View/ProfileView.swift
@@ -6,20 +6,26 @@
 //
 
 import SwiftUI
+
 struct ProfileView: View {
     @Environment(PokedexStore.self) private var store
-
+    
     private let columns = [
         GridItem(.flexible()),
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
-
+    
     var body: some View {
         NavigationStack {
-            GeometryReader { Geometry in
+            ZStack {
                 BackgroundView {
-                    ScrollView {
+                    Color.clear
+                }
+                .ignoresSafeArea()
+                
+                ScrollView {
+                    VStack(spacing: 0) {
                         ZStack {
                             RoundedRectangle(cornerRadius: 20)
                                 .fill(Color.black)
@@ -29,7 +35,7 @@ struct ProfileView: View {
                                     RoundedRectangle(cornerRadius: 20)
                                         .stroke(Color.blue.opacity(0.2), lineWidth: 1)
                                 )
-
+                            
                             HStack(spacing: 20) {
                                 ZStack {
                                     Circle()
@@ -59,80 +65,73 @@ struct ProfileView: View {
                                         .font(.footnote)
                                         .foregroundColor(.gray)
                                 }
-                                
                                 Spacer()
                             }
                             .padding(.horizontal, 30)
                         }
                         .padding(.horizontal)
+                        .padding(.top, 100)
                         .padding(.bottom, 20)
                         
                         VStack(alignment: .leading, spacing: 12) {
-                                HStack {
-                                    Image(systemName: "person.3.fill")
-                                    Text("My Team (\(store.teamPokemons.count)/6)")
-                                        .font(.title2)
-                                        .bold()
-                                }.padding(.horizontal)
+                            HStack {
+                                Image(systemName: "person.3.fill")
+                                Text("My Team (\(store.teamPokemons.count)/6)")
+                                    .font(.title2)
+                                    .bold()
+                            }.padding(.horizontal)
                             
-                                LazyVGrid(columns: columns, spacing: 16) {
-                                    if (store.teamPokemons.isEmpty) {
-                                        NavigationLink(destination: PokemonListView()) {
-                                            PokemonMiniCardEmpty(title: "Add a pokémon")
-                                        }
+                            LazyVGrid(columns: columns, spacing: 16) {
+                                if store.teamPokemons.isEmpty {
+                                    NavigationLink(destination: PokemonListView()) {
+                                        PokemonMiniCardEmpty(title: "Add a pokémon")
                                     }
-                                    else {
-                                        ForEach(store.teamPokemons) { pokemon in
-                                            NavigationLink(destination: PokemonDetailsView(pokeName: pokemon.name)) {
-                                                PokemonMiniCard(pokemon: pokemon)
-                                            }
-                                        }
-                                    }
-                                }
-                                .frame(maxWidth: .infinity, alignment: .center)
-                                .padding(.horizontal)
-                            }
-                            .padding(.vertical)
-
-
-                            VStack(alignment: .leading, spacing: 12) {
-                                HStack {
-                                    Image(systemName: "heart.fill")
-                                    Text("Favorites")
-                                        .font(.title2)
-                                        .bold()
-                                }.padding(.horizontal)
-
-
-                                LazyVGrid(columns: columns, spacing: 16) {
-                                    if (store.favoritePokemons.isEmpty) {
-                                        NavigationLink(destination: PokemonListView()) {
-                                            PokemonMiniCardEmpty(title: "Add a pokémon")
-                                        }
-                                    }
-                                    else {
-                                        ForEach(store.favoritePokemons) { pokemon in
-                                            NavigationLink(destination: PokemonDetailsView(pokeName: pokemon.name)) {
-                                                PokemonMiniCard(pokemon: pokemon)
-                                            }
+                                } else {
+                                    ForEach(store.teamPokemons) { pokemon in
+                                        NavigationLink(destination: PokemonDetailsView(pokeName: pokemon.name)) {
+                                            PokemonMiniCard(pokemon: pokemon)
                                         }
                                     }
                                 }
-                                .navigationTitle("Profile")
-                                .frame(maxWidth: .infinity, alignment: .center)
-                                .padding(.horizontal)
                             }
+                            .padding(.horizontal)
                         }
-
+                        .padding(.vertical)
+                        
+                        VStack(alignment: .leading, spacing: 12) {
+                            HStack {
+                                Image(systemName: "heart.fill")
+                                Text("Favorites")
+                                    .font(.title2)
+                                    .bold()
+                            }.padding(.horizontal)
+                            
+                            LazyVGrid(columns: columns, spacing: 16) {
+                                if store.favoritePokemons.isEmpty {
+                                    NavigationLink(destination: PokemonListView()) {
+                                        PokemonMiniCardEmpty(title: "Add a pokémon")
+                                    }
+                                } else {
+                                    ForEach(store.favoritePokemons) { pokemon in
+                                        NavigationLink(destination: PokemonDetailsView(pokeName: pokemon.name)) {
+                                            PokemonMiniCard(pokemon: pokemon)
+                                        }
+                                    }
+                                }
+                            }
+                            .padding(.horizontal)
+                        }
+                        .padding(.vertical)
+                        
                         Spacer(minLength: 40)
                     }
-                    .padding(.top)
                 }
-            } 
+            }
+            .navigationTitle("Profile")
             .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
-
 
 #Preview {
     ProfileView()
